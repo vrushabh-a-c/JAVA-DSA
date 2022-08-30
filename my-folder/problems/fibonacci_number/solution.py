16e67f5fc1627bf16b9f66d1e27359653638c803
vrushabh-a-c <115061962+vrushabh-a-c@users.noreply.github.com>
2022-08-30 11:41:19 +0000

    Sync LeetCode submission - Fibonacci Number (python)

---
+++ my-folder/problems/fibonacci_number/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<2:
+            return n
+        a,b = 0,1
+        for i in range(1,n):
+            a,b = b,a+b
+            
+        return b
+        
