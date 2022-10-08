f825b96dd3ad05a52ab7de0cb0cc52da9aa02577
vrushabh-a-c <115061962+vrushabh-a-c@users.noreply.github.com>
2022-10-08 09:13:09 +0000

    Sync LeetCode submission - 3Sum (java)

--- my-folder/problems/3sum/solution.java
+++ my-folder/problems/3sum/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
         if(nums.length<3)
-            return result;
+            return list;
         int n = nums.length;
         for(int i=0;i<n-2;i++){
             if(i>0 && nums[i]==nums[i-1])
@@ -11,9 +11,10 @@ class Solution {
             int left = i+1;
             int right = n-1;
             while(left<right){
-                int currSum=nums[i]+nums[left]+nums[right];
+                int currSum = nums[i]+nums[left]+nums[right];
                 if(currSum==0){
-                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    list.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    
                     while(left<right && nums[left]==nums[left+1])
                         left++;
                     while(left<right && nums[right]==nums[right-1])
@@ -27,6 +28,6 @@ class Solution {
                     left++;
             }
         }
-        return result;
+        return list;
     }
 }
