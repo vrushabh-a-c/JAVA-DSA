ac0c32b4b3792844d52fd36f2642b27231cf5a60
vrushabh-a-c <115061962+vrushabh-a-c@users.noreply.github.com>
2022-10-08 08:23:35 +0000

    Sync LeetCode submission - Spiral Matrix (java)

---
+++ my-folder/problems/spiral_matrix/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int top = 0;
+        int left = 0;
+        int bottom = row-1;
+        int right = col-1;
+        
+        while(top<=bottom && left<=right){
+            //for left to right
+            for(int i=left;i<=right;i++)
+                ans.add(matrix[top][i]);
+            top++;
+            
+            //for right corner to right bottom
+            for(int i=top;i<=bottom;i++)
+                ans.add(matrix[i][right]);
+            right--;
+            
+            //for bottom right corner to bottom left corner
+            if(top<=bottom){
+                for(int i=right;i>=left;i--)
+                    ans.add(matrix[bottom][i]);
+                bottom--;
+            }
+            //for left corner to top
+            if(left<=right){
+                for(int i=bottom;i>=top;i--)
+                    ans.add(matrix[i][left]);
+                left++;
+            }
+        }
+        return ans;
+    }
+}
