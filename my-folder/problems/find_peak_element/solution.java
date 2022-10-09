002e3cf8ca2a2de9999ffd162e6c0daf30a10146
vrushabh-a-c <115061962+vrushabh-a-c@users.noreply.github.com>
2022-10-09 04:41:12 +0000

    Sync LeetCode submission - Find Peak Element (java)

---
+++ my-folder/problems/find_peak_element/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]>nums[mid+1])
+                high = mid;
+            else
+                low = mid+1;
+        }
+        return low;
+    }
+}
