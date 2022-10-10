d0633f6544a225adb7bb68ead56750f01b16e1ca
vrushabh-a-c <115061962+vrushabh-a-c@users.noreply.github.com>
2022-10-10 05:07:07 +0000

    Sync LeetCode submission - Single Element in a Sorted Array (java)

---
+++ my-folder/problems/single_element_in_a_sorted_array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1]))
+                low = mid+1;
+            else
+                high = mid;
+        }
+        return nums[low];
+    }
+}
